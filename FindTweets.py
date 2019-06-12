671ce3fed49e057fb4d45872b66afd5a0b2ead18
Kumar01, Abhishek [External] <Abhishek.Kumar01@externals.adidas.com>
2019-06-12 18:44:40 +0530

    Command Line Argument Added.

--- FindTweets.py
+++ FindTweets.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     # parse arguments
     parser = argparse.ArgumentParser(description='Argument for Tweet Text')
-    parser.add_argument('-text', action="store", dest="v_text", help="Tweet Text to Search")
+    parser.add_argument('text', help="Tweet Text to Search")
     args = parser.parse_args()
 
     consumer_key = None
@@ -18,16 +18,14 @@ if __name__ == "__main__":
     access_token_secret = None
 
     # check is function is missing
-    if args.function is None:
-        raise Exception("Required arguments: -text")
-    elif args.function == "-text":
+    if args.text is None:
+        raise Exception("Required arguments: text")
+    else:
         consumer_key = input("Enter Consumer Key : ")
         consumer_secret = input("Enter Consumer Secret : ")
 
         access_token = input("Enter Access Token : ")
         access_token_secret = input("Enter Access Token Secret : ")
-    else:
-        print("Unknown service: " + args.function)
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
